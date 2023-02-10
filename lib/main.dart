91858711f77af1503c949b4d56177e9bcadc14c9
Omar Abdelwahab <Maroabdelwahab@gmail.com>
2023-02-10 15:50:02 +0200

    added sizer and providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:jobsque/screens/on_board/provider/on_board_provider.dart';
 import 'package:jobsque/screens/splash/provider/splash_provider.dart';
+import 'package:jobsque/screens/user_handling/create_account/provider/create_account_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:sizer/sizer.dart';
@@ -24,6 +25,9 @@ class MyApp extends StatelessWidget {
                 create: (context) => SplashProvider()),
             ChangeNotifierProvider<OnBoardProvider>(
               create: (context) => OnBoardProvider(),
+            ),
+            ChangeNotifierProvider<CreateAccountProvider>(
+              create: (context) => CreateAccountProvider(),
             )
           ],
           child: GestureDetector(
