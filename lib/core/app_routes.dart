fad0360658dac8f43a33d4746d18d19f510ce330
Omar Abdelwahab <Maroabdelwahab@gmail.com>
2023-02-10 15:49:28 +0200

    added new routes

--- lib/core/app_routes.dart
+++ lib/core/app_routes.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:jobsque/screens/home/home_screen.dart';
 import 'package:jobsque/screens/on_board/on_board_screen.dart';
 import 'package:jobsque/screens/splash/splash_screen.dart';
+import 'package:jobsque/screens/user_handling/create_account/create_account_screen.dart';
 
 class AppRoutes {
   static const String splash = "/";
   static const String onBoard = "onBoard";
-  static const  String home = "home";
+  static const String home = "home";
+  static const String createAccount = "createAccount";
 
   static Route<dynamic> onGenerateRoute(RouteSettings setting) {
     switch (setting.name) {
@@ -21,6 +23,10 @@ class AppRoutes {
       case home:
         return MaterialPageRoute(
           builder: (context) => const HomeScreen(),
+        );
+      case createAccount:
+        return MaterialPageRoute(
+          builder: (context) => const CreateAccountScreen(),
         );
       default:
         return MaterialPageRoute(
