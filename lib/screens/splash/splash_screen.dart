3acd16dc14e1fd4a89a6209ab9aede0c4012d28f
Omar Abdelwahab <Maroabdelwahab@gmail.com>
2023-02-10 15:52:26 +0200

    changed image pathes

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jobsque/core/assets.dart';
 import 'package:jobsque/screens/splash/provider/splash_provider.dart';
 import 'package:provider/provider.dart';
 import 'package:sizer/sizer.dart';
@@ -14,27 +15,27 @@ class SplashScreen extends StatelessWidget {
         children: [
           Center(
             child: Image.asset(
-              "assets/images/splash_screen/Ellipse 817.png", scale: 1.2.sp
+              AppAssets.ellipse1, scale: 1.2.sp
             ),
           ),
           Center(
             child: Image.asset(
-              "assets/images/splash_screen/Ellipse 816.png", scale: 1.2.sp
+              AppAssets.ellipse2, scale: 1.2.sp
             ),
           ),
           Center(
             child: Image.asset(
-              "assets/images/splash_screen/Ellipse 815.png", scale: 1.2.sp
+              AppAssets.ellipse3, scale: 1.2.sp
             ),
           ),
           Center(
             child: Image.asset(
-              "assets/images/splash_screen/Ellipse 814.png", scale: 1.2.sp
+              AppAssets.ellipse4, scale: 1.2.sp
             ),
           ),
           Center(
               child: Image.asset(
-            "assets/images/components/big logo.png",
+            AppAssets.bigLogo,
             scale: 1.2.sp,
           )),
         ],
