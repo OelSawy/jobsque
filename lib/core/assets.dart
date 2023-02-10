5ef0f3aebd11a8cd2eecb34764311a62c0a4530c
Omar Abdelwahab <Maroabdelwahab@gmail.com>
2023-02-10 15:48:53 +0200

    added images

---
+++ lib/core/assets.dart
@@ -0,0 +1,18 @@
+class AppAssets {
+  static const String ellipse1 = "assets/images/splash_screen/Ellipse 817.png";
+  static const String ellipse2 = "assets/images/splash_screen/Ellipse 816.png";
+  static const String ellipse3 = "assets/images/splash_screen/Ellipse 815.png";
+  static const String ellipse4 = "assets/images/splash_screen/Ellipse 814.png";
+  static const String bigLogo = "assets/images/components/big logo.png";
+  static const String smallLogo = "assets/images/components/small logo.png";
+  static const String googleButton =
+      "assets/images/components/google button.png";
+  static const String facebookButton =
+      "assets/images/components/facebook button.png";
+  static const String onBoardPage1 =
+      "assets/images/on_board/page 1/background.png";
+  static const String onBoardPage2 =
+      "assets/images/on_board/page 2/background.png";
+  static const String onBoardPage3 =
+      "assets/images/on_board/page 3/background.png";
+}
