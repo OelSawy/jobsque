41c15cedd3bb26659c5365b02494377b0db47c64
Omar Abdelwahab <Maroabdelwahab@gmail.com>
2023-02-12 17:34:35 +0200

    added image paths

--- lib/core/assets.dart
+++ lib/core/assets.dart
@@ -1,18 +1,45 @@
 class AppAssets {
+  //! splash
   static const String ellipse1 = "assets/images/splash_screen/Ellipse 817.png";
   static const String ellipse2 = "assets/images/splash_screen/Ellipse 816.png";
   static const String ellipse3 = "assets/images/splash_screen/Ellipse 815.png";
   static const String ellipse4 = "assets/images/splash_screen/Ellipse 814.png";
+
+  //! components
   static const String bigLogo = "assets/images/components/big logo.png";
   static const String smallLogo = "assets/images/components/small logo.png";
   static const String googleButton =
       "assets/images/components/google button.png";
   static const String facebookButton =
       "assets/images/components/facebook button.png";
+
+  //! on boarding
   static const String onBoardPage1 =
       "assets/images/on_board/page 1/background.png";
   static const String onBoardPage2 =
       "assets/images/on_board/page 2/background.png";
   static const String onBoardPage3 =
       "assets/images/on_board/page 3/background.png";
+
+  //! countries
+  static const String argentina = "assets/images/countries/argentina.png";
+  static const String brazil = "assets/images/countries/brazil.png";
+  static const String canada = "assets/images/countries/canada.png";
+  static const String china = "assets/images/countries/china.png";
+  static const String india = "assets/images/countries/india.png";
+  static const String indonesia = "assets/images/countries/indonesia.png";
+  static const String malaysia = "assets/images/countries/malaysia.png";
+  static const String philiphenes = "assets/images/countries/philiphines.png";
+  static const String poland = "assets/images/countries/poland.png";
+  static const String saudi = "assets/images/countries/saudi.png";
+  static const String singapore = "assets/images/countries/singapore.png";
+  static const String us = "assets/images/countries/us.png";
+  static const String vietnam = "assets/images/countries/vietnam.png";
+
+  //! account created successfully
+  static const String ellipse5 = "assets/images/user_handling/create_account/Ellipse 813.png";
+  static const String ellipse6 = "assets/images/user_handling/create_account/Ellipse 1.png";
+  static const String ellipse7 = "assets/images/user_handling/create_account/Ellipse 810.png";
+  static const String ellipse8 = "assets/images/user_handling/create_account/Ellipse 811.png";
+  static const String ellipse9 = "assets/images/user_handling/create_account/Ellipse 812.png";
 }
