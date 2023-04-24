60c58693afb1603c721c23c7b624fa1adb5af8ab
Omar Abdelwahab <Maroabdelwahab@gmail.com>
2023-04-24 03:46:02 +0200

    added routes

--- lib/core/app_routes.dart
+++ lib/core/app_routes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:jobsque/screens/home/home_screen.dart';
+import 'package:jobsque/screens/home/items/profile/components/edit_profile.dart';
 import 'package:jobsque/screens/on_board/on_board_screen.dart';
 import 'package:jobsque/screens/splash/splash_screen.dart';
 import 'package:jobsque/screens/user_handling/create_account/components/categories_screen.dart';
@@ -12,6 +13,9 @@ import 'package:jobsque/screens/user_handling/reset_password/components/new_pass
 import 'package:jobsque/screens/user_handling/reset_password/reset_password_screen.dart';
 
 import '../screens/home/components/home_notifications.dart';
+import '../screens/home/items/profile/components/login_and_security.dart';
+import '../screens/home/items/profile/components/notification.dart';
+import '../screens/home/items/profile/components/portfolio.dart';
 import '../screens/job_details_and_application/components/job_application/job_application_biodata.dart';
 import '../screens/job_details_and_application/components/job_application/job_application_portfolio.dart';
 import '../screens/job_details_and_application/components/job_application/job_application_successful.dart';
@@ -44,6 +48,12 @@ class AppRoutes {
   static const String applicationType = "applicationType";
   static const String applicationPortfolio = "applicationPortfolio";
   static const String applicationSuccessful = "JobApplicationSuccessful";
+
+  //! profile
+  static const String editProfile = "editProfile";
+  static const String portfolio = "portfolio";
+  static const String profileNotification = "profileNotification";
+  static const String loginAndSecurity = "loginAndSecurity";
 
   static Route<dynamic> onGenerateRoute(RouteSettings setting) {
     switch (setting.name) {
@@ -136,6 +146,26 @@ class AppRoutes {
         return MaterialPageRoute(
           settings: const RouteSettings(name: "/notificationsScreen"),
           builder: (context) => const NotificationsScreen(),
+        );
+      case editProfile:
+        return MaterialPageRoute(
+          settings: const RouteSettings(name: "/editProfile"),
+          builder: (context) => const EditProfile(),
+        );
+      case portfolio:
+        return MaterialPageRoute(
+          settings: const RouteSettings(name: "/portfolio"),
+          builder: (context) => const Portfolio(),
+        );
+      case profileNotification :
+        return MaterialPageRoute(
+          settings: const RouteSettings(name: "/profileNotification"),
+          builder: (context) => const ProfileNotification()
+        );
+      case loginAndSecurity :
+        return MaterialPageRoute(
+          settings: const RouteSettings(name: "/loginAndSecurity"),
+          builder: (context) => const LoginAndSecurity()
         );
       default:
         return MaterialPageRoute(
