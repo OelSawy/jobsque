b7a8d39c5ff785a26410ef03bf654bf5a23b2f7e
Omar Abdelwahab <Maroabdelwahab@gmail.com>
2023-07-05 01:29:19 +0300

    added urls and api routes

---
+++ lib/data/core/api_routes.dart
@@ -0,0 +1,46 @@
+import 'package:jobsque/data/core/url.dart';
+
+class ApiRoutes {
+  //! User Authentication
+  static String register = "$baseUrl/auth/register"; //? post
+  static String login = "$baseUrl/auth/login"; //? post
+  static String getOtp = "$baseUrl/auth/otp"; //? get
+  static String updateNameAndPassword = "$baseUrl/auth/user/update"; //? put
+  static String getProfile = "$baseUrl/auth/profile"; //? get
+
+  //! Profile
+  static String updateProfile = "$baseUrl/user/profile"; //? put
+  static String editBioandData = "$baseUrl/user/profile/edit"; //? put
+  static String editLanguage = "$baseUrl/user/profile/language"; //? put
+  static String addPortfolio = "$baseUrl/user/profile/portofolios"; //? post
+  static String getPortfolio = "$baseUrl/user/profile/portofolios"; //? get
+  static String editPortfolio = "$baseUrl/user/profile/portofolios"; //? put
+  static String deletePortfolio = "$baseUrl/user/profile/portofolios"; //? delete
+
+  //! Jobs
+  static String showJobs = "$baseUrl/jobs"; //? get
+  static String showSuggestedJobs = "$baseUrl/jobs/sugest"; //? get
+  static String searchJobs = "$baseUrl/jobs/search"; //? get
+  static String filterSearch = "$baseUrl/jobs/filter"; //? get
+
+  //! Favourites
+  static String addFavourite = "$baseUrl/favorites"; //? post
+  static String getFavourite = "$baseUrl/favorites"; //? get
+  static String deleteFavourite = "$baseUrl/favorites"; //? delete
+
+  //! Education
+  static String addEducation = "$baseUrl/education"; //? post
+  static String getEducation = "$baseUrl/education"; //? get
+  static String editEducation = "$baseUrl/education"; //? put
+
+  //! Experience
+  static String addExperience = "$baseUrl/experince"; //? post
+  static String endExperience = "$baseUrl/experince"; //? put
+
+  //! Apply
+  static String applyJob = "$baseUrl/apply"; //? post
+  static String reviewApply = "$baseUrl/apply"; //? put
+
+  //! Company
+  static String createCompany = "$baseUrl/company"; //? post
+}
---
+++ lib/data/core/url.dart
@@ -0,0 +1,1 @@
+String baseUrl = "http://167.71.79.133/api";
