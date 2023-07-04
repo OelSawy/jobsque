102046c4b0160811b495a6f6eb7c04a957574a37
Omar Abdelwahab <Maroabdelwahab@gmail.com>
2023-07-05 00:02:07 +0300

    misc

--- lib/screens/splash/provider/splash_provider.dart
+++ lib/screens/splash/provider/splash_provider.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:jobsque/core/app_routes.dart';
 import 'package:jobsque/screens/splash/provider/splash_state.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashProvider extends ChangeNotifier {
   SplashState state = SplashState();
 
   Future<void> init(BuildContext context) async {
-    SharedPreferences shared = await SharedPreferences.getInstance();
+    // SharedPreferences shared = await SharedPreferences.getInstance();
     await Future.delayed(const Duration(seconds: 2)).then((value) {
       //TODO: enable condition
       /* shared.containsKey("onBoard")
