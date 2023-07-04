0c3ea4f8fb659b78d496f7f04ae9d9324af286aa
Omar Abdelwahab <Maroabdelwahab@gmail.com>
2023-07-04 23:58:28 +0300

    added enums

--- lib/core/enums.dart
+++ lib/core/enums.dart
@@ -1,3 +1,5 @@
 enum ChosenNavigationItem { home, messages, applied, saved, profile }
 
 enum SelectedJobDetailsSection { description, company, people }
+
+enum VerificationMethod { sms, authenticator }
