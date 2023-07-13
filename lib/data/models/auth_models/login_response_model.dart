cfad5a2c8c794596a414fab814ca3a516f67f98c
Omar Abdelwahab <Maroabdelwahab@gmail.com>
2023-07-14 02:05:44 +0300

    added new models for approved and denied authentication

--- lib/data/models/auth_models/login_response_model.dart
+++ lib/data/models/auth_models/login_response_model.dart
@@ -4,22 +4,26 @@
 
 import 'dart:convert';
 
-LoginResponseModel loginResponseModelFromJson(String str) => LoginResponseModel.fromJson(json.decode(str));
-
-String loginResponseModelToJson(LoginResponseModel data) => json.encode(data.toJson());
-
 class LoginResponseModel {
+
+}
+
+LoginResponseModelApproved loginResponseModelApprovedFromJson(String str) => LoginResponseModelApproved.fromJson(json.decode(str));
+
+String loginResponseModelApprovedToJson(LoginResponseModelApproved data) => json.encode(data.toJson());
+
+class LoginResponseModelApproved extends LoginResponseModel {
     User user;
     String token;
     bool status;
 
-    LoginResponseModel({
+    LoginResponseModelApproved({
         required this.user,
         required this.token,
         required this.status,
     });
 
-    factory LoginResponseModel.fromJson(Map<String, dynamic> json) => LoginResponseModel(
+    factory LoginResponseModelApproved.fromJson(Map<String, dynamic> json) => LoginResponseModelApproved(
         user: User.fromJson(json["user"]),
         token: json["token"],
         status: json["status"],
@@ -73,5 +77,30 @@ class User {
         "email_verified_at": emailVerifiedAt,
         "created_at": createdAt.toIso8601String(),
         "updated_at": updatedAt.toIso8601String(),
+    };
+}
+
+
+LoginResponseModelDenied loginResponseModelDeniedFromJson(String str) => LoginResponseModelDenied.fromJson(json.decode(str));
+
+String loginResponseModelDeniedToJson(LoginResponseModelDenied data) => json.encode(data.toJson());
+
+class LoginResponseModelDenied extends LoginResponseModel {
+    String massage;
+    bool status;
+
+    LoginResponseModelDenied({
+        required this.massage,
+        required this.status,
+    });
+
+    factory LoginResponseModelDenied.fromJson(Map<String, dynamic> json) => LoginResponseModelDenied(
+        massage: json["massage"],
+        status: json["status"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "massage": massage,
+        "status": status,
     };
 }
--- lib/data/models/auth_models/register_response_model.dart
+++ lib/data/models/auth_models/register_response_model.dart
@@ -1,27 +1,29 @@
 // To parse this JSON data, do
 //
-//     final registerResponseModel = registerResponseModelFromJson(jsonString);
+//     final registerResponseModelApproved = registerResponseModelApprovedFromJson(jsonString);
 
 import 'dart:convert';
 
-RegisterResponseModel registerResponseModelFromJson(String str) => RegisterResponseModel.fromJson(json.decode(str));
+class RegisterResponseModel {}
 
-String registerResponseModelToJson(RegisterResponseModel data) => json.encode(data.toJson());
+RegisterResponseModelApproved registerResponseModelApprovedFromJson(String str) => RegisterResponseModelApproved.fromJson(json.decode(str));
 
-class RegisterResponseModel {
+String registerResponseModelApprovedToJson(RegisterResponseModelApproved data) => json.encode(data.toJson());
+
+class RegisterResponseModelApproved extends RegisterResponseModel{
     bool status;
     Data data;
     Data profile;
     String token;
 
-    RegisterResponseModel({
+    RegisterResponseModelApproved({
         required this.status,
         required this.data,
         required this.profile,
         required this.token,
     });
 
-    factory RegisterResponseModel.fromJson(Map<String, dynamic> json) => RegisterResponseModel(
+    factory RegisterResponseModelApproved.fromJson(Map<String, dynamic> json) => RegisterResponseModelApproved(
         status: json["status"],
         data: Data.fromJson(json["data"]),
         profile: Data.fromJson(json["profile"]),
@@ -73,5 +75,46 @@ class Data {
         "created_at": createdAt.toIso8601String(),
         "id": id,
         "user_id": userId,
+    };
+}
+
+
+RegisterResponseModelDenied registerResponseModelDeniedFromJson(String str) => RegisterResponseModelDenied.fromJson(json.decode(str));
+
+String registerResponseModelDeniedToJson(RegisterResponseModelDenied data) => json.encode(data.toJson());
+
+class RegisterResponseModelDenied extends RegisterResponseModel {
+    Massege massege;
+    bool status;
+
+    RegisterResponseModelDenied({
+        required this.status,
+        required this.massege,
+    });
+
+    factory RegisterResponseModelDenied.fromJson(Map<String, dynamic> json) => RegisterResponseModelDenied(
+        status: json["status"],
+        massege: Massege.fromJson(json["massege"]),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "status": status,
+        "massege": massege.toJson(),
+    };
+}
+
+class Massege {
+    List<String> email;
+
+    Massege({
+        required this.email,
+    });
+
+    factory Massege.fromJson(Map<String, dynamic> json) => Massege(
+        email: List<String>.from(json["email"].map((x) => x)),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "email": List<dynamic>.from(email.map((x) => x)),
     };
 }
