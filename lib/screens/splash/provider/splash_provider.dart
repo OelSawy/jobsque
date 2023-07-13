9be7cee042716c642c564112a14656668bc8a59f
Omar Abdelwahab <Maroabdelwahab@gmail.com>
2023-07-14 02:07:43 +0300

    misc

--- lib/screens/splash/provider/splash_provider.dart
+++ lib/screens/splash/provider/splash_provider.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:jobsque/core/app_routes.dart';
+import 'package:jobsque/screens/home/provider/home_provider.dart';
 import 'package:jobsque/screens/splash/provider/splash_state.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashProvider extends ChangeNotifier {
   SplashState state = SplashState();
 
   Future<void> init(BuildContext context) async {
-    // SharedPreferences shared = await SharedPreferences.getInstance();
-    await Future.delayed(const Duration(seconds: 2)).then((value) {
-      //TODO: enable condition
-      /* shared.containsKey("onBoard")
-          ? Navigator.of(context)
-              .pushNamedAndRemoveUntil(AppRoutes.home, (route) => false)
-          :  */Navigator.of(context)
-              .pushNamedAndRemoveUntil(AppRoutes.onBoard, (route) => false);
+    SharedPreferences shared = await SharedPreferences.getInstance();
+    await Future.delayed(const Duration(seconds: 2)).then((value) async {
+      if (shared.containsKey("onBoard")) {
+        if (shared.containsKey("registered")) {
+          if (shared.containsKey("loggedIn")) {
+            await HomeProvider().init();
+            Navigator.pushReplacementNamed(context, AppRoutes.home);
+          } else {
+            Navigator.pushReplacementNamed(context, AppRoutes.login);
+          }
+        } else {
+          Navigator.pushReplacementNamed(context, AppRoutes.createAccount);
+        }
+      } else {
+        Navigator.pushReplacementNamed(context, AppRoutes.onBoard);
+      }
     });
   }
 }
