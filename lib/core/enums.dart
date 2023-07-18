0806ad6fbbc2fc2ed15f862c10a7186bcb3d7893
Omar Abdelwahab <Maroabdelwahab@gmail.com>
2023-07-18 23:04:42 +0300

    added enums for loading states

--- lib/core/enums.dart
+++ lib/core/enums.dart
@@ -3,3 +3,7 @@ enum ChosenNavigationItem { home, messages, applied, saved, profile }
 enum SelectedJobDetailsSection { description, company, people }
 
 enum VerificationMethod { sms, authenticator }
+
+enum LoadingState { initial, loading, error, done }
+
+enum FilePicking { choosing, error, done, initial }
