468dbd94c9e78c99d502f5b7c5f067f5b5ef029b
Omar Abdelwahab <maroabdelwahab@gmail.com>
2023-08-21 19:37:56 +0300

    update portofolio service

--- lib/data/services/profile_services/portfolio_services.dart
+++ lib/data/services/profile_services/portfolio_services.dart
@@ -1,32 +1,17 @@
-import 'dart:io';
-
 import 'package:http/http.dart';
 import 'package:http/http.dart' as http;
 import 'package:jobsque/data/core/api_routes.dart';
-import 'package:jobsque/data/models/portfolio_models/add_portfolio_response_model.dart';
 import 'package:jobsque/data/models/portfolio_models/delete_portfolio_response_model.dart';
 import 'package:jobsque/data/models/portfolio_models/edit_portfolio_response_model.dart';
 import 'package:jobsque/data/models/portfolio_models/get_portfolio_response_model.dart';
 
 class PortfolioServices {
-  Future<AddPorfolioResponseModel> addPortfolio(
-      String name, File file, String id, String token) async {
-    Response response = await http.post(
-        Uri.parse("${ApiRoutes.addPortfolio}/$id"),
-        headers: {"Authorization": "Bearer $token"},
-        body: {"cv_file": file.path, "name": name});
-
-    AddPorfolioResponseModel addPorfolioResponseModel =
-        addPorfolioResponseModelFromJson(response.body);
-    return addPorfolioResponseModel;
-  }
-
   Future<EditPorfolioResponseModel> editPortfolio(
-      String name, String path, String id, String token) async {
+      String name, String id, String token) async {
     Response response = await http.put(
         Uri.parse("${ApiRoutes.editPortfolio}/$id"),
         headers: {"Authorization": "Bearer $token"},
-        body: {"cv_file": path, "name": name});
+        body: {"go" : "go to", "name": name});
     EditPorfolioResponseModel editPorfolioResponseModel =
         editPorfolioResponseModelFromJson(response.body);
     return editPorfolioResponseModel;
