4839307df1c6789fcff0f7aad631ad77e90345d1
Omar Abdelwahab <maroabdelwahab@gmail.com>
2023-08-21 19:49:26 +0300

    add functions for search and applied jobs

--- lib/screens/home/provider/home_provider.dart
+++ lib/screens/home/provider/home_provider.dart
@@ -1,9 +1,12 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:jobsque/core/enums.dart';
-import 'package:jobsque/data/models/auth_models/login_response_model.dart';
 import 'package:jobsque/data/models/favorites_models/get_favorite_response_model.dart';
 import 'package:jobsque/data/models/job_models/datum.dart';
+import 'package:jobsque/data/services/auth_services/profile_services.dart';
 import 'package:jobsque/data/services/favorites_services/favorites_services.dart';
+import 'package:jobsque/data/services/job_application_services/job_application_services.dart';
 import 'package:jobsque/data/services/job_services/job_services.dart';
 import 'package:jobsque/screens/home/components/home_applied.dart';
 import 'package:jobsque/screens/home/components/home_messages.dart';
@@ -13,6 +16,7 @@ import 'package:jobsque/screens/home/provider/home_state.dart';
 import 'package:restart_app/restart_app.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import '../../../data/models/auth_models/get_profile_response_model.dart';
 import '../../job_details_and_application/components/company_people.dart';
 import '../../job_details_and_application/components/company_info.dart';
 import '../../job_details_and_application/components/job_description.dart';
@@ -106,17 +110,26 @@ class HomeProvider extends ChangeNotifier {
     notifyListeners();
     SharedPreferences shared = await SharedPreferences.getInstance();
     if (shared.containsKey("profile")) {
-      state.profile =
-          loginResponseModelApprovedFromJson(shared.getString("profile")!).user;
+      state.profile = userFromJson(shared.getString("profile")!);
     }
     state.showJobsResponseModel =
         await JobServices().getJobs(shared.getString("token")!);
     state.recentJobs = state.showJobsResponseModel!.data;
-    state.showSuggestedJobsResponseModel = await JobServices()
+    /* state.showSuggestedJobsResponseModel = await JobServices()
         .showSuggestedJobs(shared.getString("id")!, shared.getString("token")!);
-    state.suggestedJobs = state.showSuggestedJobsResponseModel!.data;
+    state.suggestedJobs = state.showSuggestedJobsResponseModel!.data; */
     getSaved();
+    getApplied();
     state.loadingState = LoadingState.done;
+    notifyListeners();
+  }
+
+  Future<void> getApplied() async {
+    state.appliedLoadingState = LoadingState.loading;
+    notifyListeners();
+    SharedPreferences shared = await SharedPreferences.getInstance();
+    state.appliedJobs = (await JobApplicationServices().getAppliedJobs(shared.getString("id")!, shared.getString("token")!)).data;
+    state.appliedLoadingState = LoadingState.done;
     notifyListeners();
   }
 
@@ -127,7 +140,11 @@ class HomeProvider extends ChangeNotifier {
     GetFavoriteResponseModel getFavoriteResponseModel =
         await FavoritesServices()
             .getFavorite(shared.getString("id")!, shared.getString("token")!);
-    state.savedJobs = getFavoriteResponseModel.data;
+    if (getFavoriteResponseModel.runtimeType ==
+        GetFavoriteResponseModelApproved) {
+      state.savedJobs =
+          (getFavoriteResponseModel as GetFavoriteResponseModelApproved).data;
+    }
     state.savedLoadingState = LoadingState.done;
     notifyListeners();
   }
@@ -157,4 +174,76 @@ class HomeProvider extends ChangeNotifier {
     getSaved();
     notifyListeners();
   }
+
+  void resetFilters() {
+    state.jobTitleValue = 0;
+    state.locationValue = 0;
+    state.salaryValue = 0;
+    notifyListeners();
+  }
+
+  void jobTitleValueChanged(int? value) {
+    state.jobTitleValue = value!;
+    notifyListeners();
+  }
+
+  void locationValueChanged(int? value) {
+    state.locationValue = value!;
+    notifyListeners();
+  }
+
+  void salaryValueChanged(int? value) {
+    state.salaryValue = value!;
+    notifyListeners();
+  }
+
+  void jobTypeValueChanged(int? value) {
+    state.jobTypeValue == value
+        ? state.jobTypeValue = 0
+        : state.jobTypeValue = value!;
+    notifyListeners();
+  }
+
+  void jobTimeValueChanged(int? value) {
+    state.jobTimeValue == value
+        ? state.jobTimeValue = 0
+        : state.jobTimeValue = value!;
+    notifyListeners();
+  }
+
+  void jobLocationValueChanged(int? value) {
+    state.jobLocationValue == value
+        ? state.jobLocationValue = 0
+        : state.jobLocationValue = value!;
+    notifyListeners();
+  }
+
+  void datePostedValueChanged(int? value) {
+    state.datePostedValue == value
+        ? state.datePostedValue = 0
+        : state.datePostedValue = value!;
+    notifyListeners();
+  }
+
+  Future<void> updateProfile() async {
+    SharedPreferences shared = await SharedPreferences.getInstance();
+    state.loadingState = LoadingState.loading;
+    notifyListeners();
+    state.profile = (await ProfileServices()
+            .getProfile(shared.getString("id")!, shared.getString("token")!))
+        .data;
+    shared.setString("profile", json.encode(state.profile.toJson()));
+    state.loadingState = LoadingState.done;
+    notifyListeners();
+  }
+
+  Future<void> search(String query) async {
+    state.results = state.recentJobs
+        .where((element) =>
+            (element).name.toLowerCase().contains(query.toLowerCase()))
+        .toList();
+    notifyListeners();
+  }
+
+  void filterResults() {}
 }
